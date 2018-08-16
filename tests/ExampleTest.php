ea5bf3bb7292f58a57f7b1892ebeb5bf1e7175ba
Neil Charlton <neil.charlton@extreme-creations.co.uk>
2018-08-16 14:55:22 +0100

    Tests and config

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace League\Skeleton;
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
